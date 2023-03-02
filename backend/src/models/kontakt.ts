ae751d35bb49e6c447ec65bcedb476c92c46bdf4
almoulki <tamim.almoulki@gmail.com>
2023-03-02 10:31:35 +0100

    Post und Delete funktieren mit klein Buchstabieren für Tabletete und Columens Name in DB

--- backend/src/models/kontakt.ts
+++ backend/src/models/kontakt.ts
@@ -69,13 +69,13 @@ export class Kontakt {
   }
 
    public async save() {
-    const createQueryString = 'INSERT INTO DB_ADR ("Vorname1", "Name", "strsseNr", "landPLZ", "ort", "telefon", "email1", "email2", "telExtra1", "telExtra2", "telExtra3", "spitzname", "vorname2", geburtstag, hochzeitstag, kennengelerrnt, "woKennengelernt", "gemeinsamkeiten", "lieblingsGetraenk", "lieblingsAktion", "hobby1", "hobby2", "linkAccount", "xingAccount", "facebookAccount", "partner", "freunde", "kollege", "familie") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29)';
+    const createQueryString = 'INSERT INTO db_adr ("vorname1", "name", "strasseNr", "landPLZ", "ort", "telefon", "email1", "email2", "telExtra1", "telExtra2", "telExtra3", "spitzname", "vorname2", "geburtstag", "hochzeitstag", "kennengelernt", "woKennengelernt", "gemeinsamkeiten", "lieblingsGetraenk", "lieblingsAktion", "hobby1", "hobby2", "linkAccount", "xingAccount", "facebookAccount", "partner", "freunde", "kollege", "familie") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29) RETURNING *';
     const res = await pgClient.query(createQueryString, [this.vorname1, this.name, this.strasseNr, this.landPLZ, this.ort, this.telefon, this.email1, this.email2, this.telExtra1, this.telExtra2, this.telExtra3, this.spitzname, this.vorname2, this.geburtstag, this.hochzeitstag, this.kennengelernt, this.woKennengelernt, this.gemeinsamkeiten, this.lieblingsGetraenk, this.lieblingsAktion, this.hobby1, this.hobby2, this.linkAcount, this.xingAccount, this.facebookAccount, this.partner, this.freunde, this.kollege, this.familie]);
     this.adressID = res.rows[0].adressID;
   }
 
   public async delete() {
-    const result = await pgClient.query(`DELETE FROM DB_ADR name = $1 and vorname1 = $2`, [this.name, this.vorname1]);
+    const result = await pgClient.query(`DELETE FROM db_adr WHERE name = $1 and vorname1 = $2`, [this.name, this.vorname1]);
 
   }
 }
